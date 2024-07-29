5fd713d6a72b651fd1e90286b7155a4ef40e8e2d
AVVKavvk <kumawatvipin066@gmail.com>
2024-07-29 16:03:05 +0530

    created go routin and used waitGroup

---
+++ routin/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+var wg sync.WaitGroup;
+func main() {
+
+	websites := []string{
+		"https://go.dev",
+		"https://fb.com",
+		"https://vipinnotes.onrender.com",
+		"https://github.com",
+		"https://google.com",
+	}
+
+	for _,web:= range websites{
+		go getDetailsOfWebsite(web);
+		wg.Add(1)
+	}
+
+	wg.Wait();
+}
+
+func getDetailsOfWebsite(website string) {
+	res,err:=http.Get(website);
+
+	if err!=nil {
+		log.Fatal(err);
+	}else{
+		fmt.Printf("%v is the statuscode for %s \n",res.StatusCode,website);
+	}
+	defer wg.Done()
+}
