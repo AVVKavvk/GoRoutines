8f4afc0646153b8bc766f02cfcefe69004e22ef7
AVVKavvk <kumawatvipin066@gmail.com>
2024-07-29 18:18:00 +0530

    channels

---
+++ channels/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fmt.Println("go channels");
+	var myCha= make( chan int,1);
+
+	// myCha<-5;
+	// fmt.Println(<-myCha)
+
+	wg:=& sync.WaitGroup{};
+
+	wg.Add(2);
+	go func (ch chan int , wg *sync.WaitGroup){
+
+		fmt.Println(<-myCha);
+		fmt.Println(<-myCha);
+		wg.Done();
+	}(myCha,wg);
+	go func (ch chan int , wg *sync.WaitGroup){
+		myCha<-5;
+		myCha<-6;
+		myCha<-7;
+		wg.Done();
+	}(myCha,wg);
+
+	wg.Wait();
+}
